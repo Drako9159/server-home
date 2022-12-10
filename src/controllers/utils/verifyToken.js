d3afdc7179da9d89516f21af445883156f93eaee
drako9159 <drako9159@gmail.com>
2022-12-10 17:17:58 -0600

    await configure toast alert login

--- src/controllers/utils/verifyToken.js
+++ src/controllers/utils/verifyToken.js
@@ -2,13 +2,13 @@ const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
   if (!req.headers.cookie) {
-    return res.status(403).send({ auth: false, message: "No token provided." });
+    return res.status(403).render("alerts/alert.ejs");
   }
   const token = req.headers.cookie.split("myTokenName=")[1];
   /*
   const token = req.headers["x-access-token"]; */
   if (!token) {
-    return res.status(401).json({ auth: false, message: "No token provided." });
+    return res.status(401).render("alerts/alert.ejs");
   }
   const decoded = jwt.verify(token, "secret");
 
